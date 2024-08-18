d53ba7daa8a0d370edff27792d47e6f2b26cc425
sara-samy <sarrasamyy@icloud.com>
2024-08-19 01:56:48 +0200

    add flake.nix and lock

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Personal collection of flake templates";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    templates = {
+      cpp = {
+        path = ./templates/cpp;
+        description = "C++ development and build project with CMake";
+      };
+    };
+  };
+}
